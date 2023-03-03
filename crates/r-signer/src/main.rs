1a73d6b96d39f8a94b5db4e79d77ec592a68ac4d
Usman Saleem <usman@usmans.info>
2023-03-03 23:12:40 +1000

    feat!: Rocket based server

--- crates/r-signer/src/main.rs
+++ crates/r-signer/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/upcheck")]
+fn upcheck() -> &'static str {
+    "OK"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![upcheck])
 }
