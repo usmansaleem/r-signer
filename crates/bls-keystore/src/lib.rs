347bc592d6cfa5d5aca967fd5e60dcf79d1d5c0f
Usman Saleem <usman@usmans.info>
2023-03-05 12:35:20 +1000

    WIP: Implement decrypt for bls keystore

--- crates/bls-keystore/src/lib.rs
+++ crates/bls-keystore/src/lib.rs
@@ -1,5 +1,23 @@
+//! Library to encrypt and decrypt BLS12-381 Keystores.
+//! The keystore is in JSON format as defined by [EIP-2335][1].
+//!
+//![1]: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2335.md
+
 #[cfg(test)]
 mod tests;
+
+use unicode_normalization::UnicodeNormalization;
+
+/// Decrypt BLS12-381 keystore with provided password. Returns decrypted key
+/// as hex string.
+pub fn decrypt(keystore_json: String, password: String) -> Result<String, String> {
+    let n_password = normalize_password(password);
+    return Ok(String::from("0x0"));
+}
+
+fn normalize_password(password: String) -> String {
+    password.nfkd().collect::<String>()
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
--- crates/bls-keystore/src/tests.rs
+++ crates/bls-keystore/src/tests.rs
@@ -1,7 +1,8 @@
 use super::*;
 
 #[test]
-fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+fn normalize_works() {
+    let input = String::from("åççèñt");
+    let result = normalize_password(input.clone());
+    assert_ne!(result, input);
 }
