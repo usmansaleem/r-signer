b1c7d1cd13374620ff0dcab30e022446c40b508a
Usman Saleem <usman@usmans.info>
2023-03-07 14:23:01 +1000

    lower test complexity

--- crates/bls-keystore/src/tests.rs
+++ crates/bls-keystore/src/tests.rs
@@ -36,40 +36,50 @@ fn normalize_works_with_non_control_char() {
 }
 
 #[test]
-fn keystore_json_scrypt_parsed() {
-    let keystore_json = r#"{
-            "crypto": {
-                "kdf": {
-                    "function": "scrypt",
-                    "params": {
-                        "dklen": 32,
-                        "n": 262144,
-                        "p": 1,
-                        "r": 8,
-                        "salt": "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"
-                    },
-                    "message": ""
-            },
-            "checksum": {
-                "function": "sha256",
-                "params": {},
-                "message": "d2217fe5f3e9a1e34581ef8a78f7c9928e436d36dacc5e846690a5581e8ea484"
-            },
-            "cipher": {
-                "function": "aes-128-ctr",
-                "params": {
-                    "iv": "264daa3f303d7259501c93d997d84fe6"
-                },
-                "message": "06ae90d55fe0a6e9c5c3bc5b170827b2e5cce3929ed3f116c2811e6366dfe20f"
-            }
-            },
-            "description": "This is a test keystore that uses scrypt to secure the secret.",
-            "pubkey": "9612d7a727c9d0a22e185a1c768478dfe919cada9266988cb32359c11f2b7b27f4ae4040902382ae2910c15e2b420d07",
-            "path": "m/12381/60/3141592653/589793238",
-            "uuid": "1d85ae20-35c5-4611-98e8-aa14a633906f",
-            "version": 4
-            }"#;
+fn password_verified() {
+    let keystore_json = r#"
+    {
+      "crypto" : {
+        "kdf" : {
+          "function" : "scrypt",
+          "params" : {
+            "dklen" : 32,
+            "n" : 512,
+            "p" : 1,
+            "r" : 8,
+            "salt" : "d4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"
+          },
+          "message" : ""
+        },
+        "checksum" : {
+          "function" : "sha256",
+          "params" : { },
+          "message" : "28aea7510466a76c848c2f48649b94bd0170f90badf05480948304838d43acfc"
+        },
+        "cipher" : {
+          "function" : "aes-128-ctr",
+          "params" : {
+            "iv" : "264daa3f303d7259501c93d997d84fe6"
+          },
+          "message" : "5f4cbeea80336bd076e8f648d4c6c0f6954ae40babdbab70079fc1cd8bec4a11"
+        }
+      },
+      "pubkey" : "9612d7a727c9d0a22e185a1c768478dfe919cada9266988cb32359c11f2b7b27f4ae4040902382ae2910c15e2b420d07",
+      "version" : 4,
+      "path" : "m/12381/60/3141592653/589793238",
+      "uuid" : "eb329e94-6d98-4999-a773-6162fa0dd13a"
+    }"#;
 
-    let _keystore = decrypt(keystore_json, "password");
-    //TODO: Assert valid password
+    let normalized_password = normalize_password("𝔱𝔢𝔰𝔱𝔭𝔞𝔰𝔰𝔴𝔬𝔯𝔡🔑");
+    let keystore = keystore::parse_keystore(keystore_json).unwrap();
+    let decryption_key = keystore
+        .crypto
+        .kdf
+        .decryption_key(normalized_password.as_str())
+        .unwrap();
+    let cipher_message = hex::decode(keystore.crypto.cipher.message).unwrap();
+    let checksum_message = hex::decode(keystore.crypto.checksum.message).unwrap();
+
+    let valid_password = validate_password(&decryption_key, &cipher_message, &checksum_message);
+    assert!(valid_password);
 }
