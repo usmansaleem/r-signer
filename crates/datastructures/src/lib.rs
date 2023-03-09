f32cc61c7e24cd54ec71e576f720a19f5ecf3995
Usman Saleem <usman@usmans.info>
2023-03-09 12:56:36 +1000

    feat: Computer domain root

--- crates/datastructures/src/lib.rs
+++ crates/datastructures/src/lib.rs
@@ -3,3 +3,37 @@
 #[cfg(test)]
 mod tests;
 
+use anyhow::{bail, Result};
+use ssz_rs::prelude::*;
+
+pub type Bytes4 = [u8; 4];
+pub type Bytes32 = [u8; 32];
+
+#[derive(PartialEq, Eq, Debug, Default, Clone, SimpleSerialize)]
+struct ForkData {
+    current_version: Vector<u8, 4>,
+    genesis_validators_root: Vector<u8, 32>,
+}
+
+pub fn compute_domain(
+    domain_type: &Vec<u8>,
+    fork_version: &Vec<u8>,
+    genesis_validators_root: &Vec<u8>,
+) -> Result<Vec<u8>> {
+    let current_version_vec = match Vector::<u8, 4>::try_from(fork_version.to_vec()) {
+        Ok(val) => val,
+        Err(_) => bail!("Error creating vector from fork_version"),
+    };
+
+    let genesis_root_vec = match Vector::<u8, 32>::try_from(genesis_validators_root.to_vec()) {
+        Ok(val) => val,
+        Err(_) => bail!("Error creating vector from genesis_validators_root"),
+    };
+
+    let mut fork_data = ForkData {
+        current_version: current_version_vec,
+        genesis_validators_root: genesis_root_vec,
+    };
+    let fork_data_root = fork_data.hash_tree_root()?;
+    Ok([&domain_type[..], &fork_data_root.as_ref()[..28]].concat())
+}
--- crates/datastructures/src/tests.rs
+++ crates/datastructures/src/tests.rs
@@ -1,2 +1,16 @@
-//use super::*;
+use super::*;
 
+#[test]
+fn compute_domain_works() {
+    let domain_type = hex::decode("03000000").unwrap();
+    let fork_version = hex::decode("00000001").unwrap();
+    let genesis_validators_root =
+        hex::decode("0000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+    let domain_root =
+        compute_domain(&domain_type, &fork_version, &genesis_validators_root).unwrap();
+    assert_eq!(
+        hex::encode(domain_root),
+        "0300000018ae4ccbda9538839d79bb18ca09e23e24ae8c1550f56cbb3d84b053"
+    );
+}
