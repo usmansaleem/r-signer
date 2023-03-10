b1acd64a37477063a2650336d2ef63e749ea291f
Usman Saleem <usman@usmans.info>
2023-03-10 11:41:45 +1000

    feat: compute domain

--- crates/datastructures/src/lib.rs
+++ crates/datastructures/src/lib.rs
@@ -3,7 +3,7 @@
 #[cfg(test)]
 mod tests;
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use ssz_rs::prelude::*;
 
 pub type Bytes4 = [u8; 4];
@@ -11,28 +11,18 @@ pub type Bytes32 = [u8; 32];
 
 #[derive(PartialEq, Eq, Debug, Default, Clone, SimpleSerialize)]
 struct ForkData {
-    current_version: Vector<u8, 4>,
-    genesis_validators_root: Vector<u8, 32>,
+    current_version: Bytes4,
+    genesis_validators_root: Bytes32,
 }
 
 pub fn compute_domain(
-    domain_type: &Vec<u8>,
-    fork_version: &Vec<u8>,
-    genesis_validators_root: &Vec<u8>,
+    domain_type: &Bytes4,
+    fork_version: &Bytes4,
+    genesis_validators_root: &Bytes32,
 ) -> Result<Vec<u8>> {
-    let current_version_vec = match Vector::<u8, 4>::try_from(fork_version.to_vec()) {
-        Ok(val) => val,
-        Err(_) => bail!("Error creating vector from fork_version"),
-    };
-
-    let genesis_root_vec = match Vector::<u8, 32>::try_from(genesis_validators_root.to_vec()) {
-        Ok(val) => val,
-        Err(_) => bail!("Error creating vector from genesis_validators_root"),
-    };
-
     let mut fork_data = ForkData {
-        current_version: current_version_vec,
-        genesis_validators_root: genesis_root_vec,
+        current_version: *fork_version,
+        genesis_validators_root: *genesis_validators_root,
     };
     let fork_data_root = fork_data.hash_tree_root()?;
     Ok([&domain_type[..], &fork_data_root.as_ref()[..28]].concat())
--- crates/datastructures/src/tests.rs
+++ crates/datastructures/src/tests.rs
@@ -1,16 +1,17 @@
 use super::*;
+use hex_literal::hex;
 
 #[test]
 fn compute_domain_works() {
-    let domain_type = hex::decode("03000000").unwrap();
-    let fork_version = hex::decode("00000001").unwrap();
+    let domain_type = hex!("03000000");
+    let fork_version = hex!("00000001");
     let genesis_validators_root =
-        hex::decode("0000000000000000000000000000000000000000000000000000000000000000").unwrap();
+        hex!("0000000000000000000000000000000000000000000000000000000000000000");
 
     let domain_root =
         compute_domain(&domain_type, &fork_version, &genesis_validators_root).unwrap();
     assert_eq!(
-        hex::encode(domain_root),
-        "0300000018ae4ccbda9538839d79bb18ca09e23e24ae8c1550f56cbb3d84b053"
+        domain_root,
+        hex!("0300000018ae4ccbda9538839d79bb18ca09e23e24ae8c1550f56cbb3d84b053")
     );
 }
